b9bd2e44ac976223a1fd07098664268fff59ea00
Anuswar_M <anuswarmahapatra@gmail.com>
2023-08-07 17:04:29 +0530

    Add Sign In Page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Home from "./Home";
+import Login from "./Login"
 import Footer from "./Footer";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Home />
+      <Login />
       <Footer />
     </div>
   );
