9a6cbc0bbd7064ba4b9e5e811d96694e3936b761
Anuswar_M <anuswarmahapatra@gmail.com>
2023-08-07 14:02:59 +0530

    Complete Navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="main-navbar">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            Auction Junction
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav ms-auto">
+              <a className="nav-link" href="#">
+                Lots
+              </a>
+              <a className="nav-link" href="#">
+                Bid and Buy
+              </a>
+              <a className="nav-link" href="#">
+                Bid Status
+              </a>
+              <a className="nav-link" href="#">
+                About
+              </a>
+              <a className="nav-link" href="#">
+                Profile
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
